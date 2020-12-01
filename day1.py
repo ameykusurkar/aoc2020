288d3d292f47e2119599aadc1122d76b9d1b2083
Amey Kusurkar <amey1000@gmail.com>
2020-12-01 23:25:18 +0000

    Solve day 1

---
+++ day1.py
@@ -0,0 +1,23 @@
+import sys
+
+def findn(entries, target, n):
+    """
+    Finds `n` numbers in `entries` that sum to `target`
+    """
+    if n == 1:
+        return (target,) if target in entries else None
+    else:
+        for e in entries:
+            remaining = target - e
+            remaining_entries = entries - {e}
+            match = findn(remaining_entries, remaining, n - 1)
+            if match:
+                return (e, *match)
+
+entries = set(map(int, sys.stdin))
+
+a, b = findn(entries, 2020, 2)
+print(a * b)
+
+a, b, c = findn(entries, 2020, 3)
+print(a * b * c)
